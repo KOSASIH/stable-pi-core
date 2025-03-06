b92903ef926d64b1f98195a93fc011295eebae1f
KOSASIH <kosasihg88@gmail.com>
2025-03-06 08:26:23 +0700

    Update index.js

--- src/api/index.js
+++ src/api/index.js
@@ -1,10 +1,9 @@
-// src/api/index.js
-
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const { deposit, convert, checkBalance } = require('./controllers/conversionController');
 const bridgeRoutes = require('./routes/bridgeRoutes'); // Import the bridge routes
+const merchantRoutes = require('./routes/merchantRoutes'); // Import the merchant routes
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -20,6 +19,9 @@ app.get('/api/balance', checkBalance);
 
 // Bridge routes
 app.use('/api/bridge', bridgeRoutes); // Use the bridge routes under /api/bridge
+
+// Merchant routes
+app.use('/api/merchants', merchantRoutes); // Use the merchant routes under /api/merchants
 
 // Start the server
 app.listen(PORT, () => {
