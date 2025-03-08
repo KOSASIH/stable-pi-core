5ce3d8dd57a906cbbb2b1df63016849f4c4e6dae
KOSASIH <kosasihg88@gmail.com>
2025-03-08 13:16:35 +0700

    Create server.js

---
+++ token-incentive-system/config/server.js
@@ -0,0 +1,30 @@
+// config/server.js
+
+const express = require('express');
+const connectDB = require('./db');
+const web3 = require('./web3');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Connect to MongoDB
+connectDB();
+
+// Sample route
+app.get('/', (req, res) => {
+    res.send('API is running...');
+});
+
+// Start the server
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
+
+// Export the app for testing purposes
+module.exports = app;
