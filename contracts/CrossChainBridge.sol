4a4421a7cf8dfbdd0b683ca66b0dfd7c8bc7b0e7
KOSASIH <kosasihg88@gmail.com>
2025-03-13 20:50:22 +0700

    Update CrossChainBridge.sol

--- contracts/CrossChainBridge.sol
+++ contracts/CrossChainBridge.sol
@@ -4,14 +4,18 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 
 contract CrossChainBridge is AccessControl, ReentrancyGuard {
+    using ECDSA for bytes32;
+
     bytes32 public constant ADMIN_ROLE = keccak256("ADMIN_ROLE");
     bytes32 public constant OPERATOR_ROLE = keccak256("OPERATOR_ROLE");
 
     event TransferInitiated(address indexed from, address indexed to, uint256 amount, string targetChain, address token);
     event TransferCompleted(address indexed to, uint256 amount, string sourceChain, address token);
     event TransferFailed(bytes32 indexed transferId, string reason);
+    event OperatorUpdated(address indexed operator, bool isActive);
 
     struct Transfer {
         address from;
@@ -23,6 +27,7 @@ contract CrossChainBridge is AccessControl, ReentrancyGuard {
     }
 
     mapping(bytes32 => Transfer) public transfers;
+    mapping(address => bool) public operators;
 
     constructor() {
         _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
@@ -31,8 +36,13 @@ contract CrossChainBridge is AccessControl, ReentrancyGuard {
     }
 
     modifier onlyOperator() {
-        require(hasRole(OPERATOR_ROLE, msg.sender), "Not an operator");
+        require(operators[msg.sender], "Not an operator");
         _;
+    }
+
+    function setOperator(address _operator, bool _isActive) external onlyRole(ADMIN_ROLE) {
+        operators[_operator] = _isActive;
+        emit OperatorUpdated(_operator, _isActive);
     }
 
     function initiateTransfer(address _to, uint256 _amount, string calldata _targetChain, address _token) external nonReentrant {
@@ -46,9 +56,14 @@ contract CrossChainBridge is AccessControl, ReentrancyGuard {
         emit TransferInitiated(msg.sender, _to, _amount, _targetChain, _token);
     }
 
-    function completeTransfer(bytes32 _transferId) external onlyOperator nonReentrant {
+    function completeTransfer(bytes32 _transferId, bytes calldata _signature) external onlyOperator nonReentrant {
         Transfer storage transfer = transfers[_transferId];
         require(!transfer.completed, "Transfer already completed");
+
+        // Verify the signature
+        bytes32 messageHash = keccak256(abi.encodePacked(_transferId));
+        address signer = messageHash.toEthSignedMessageHash().recover(_signature);
+        require(hasRole(OPERATOR_ROLE, signer), "Invalid signature");
 
         transfer.completed = true;
         require(IERC20(transfer.token).transfer(transfer.to, transfer.amount), "Transfer to recipient failed");
