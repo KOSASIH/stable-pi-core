c081141d5a89e47d86292a2da093ef53653daa0f
KOSASIH <kosasihg88@gmail.com>
2025-03-15 20:26:33 +0700

    Update adaptiveConsensus.js

--- consensus/adaptiveConsensus.js
+++ consensus/adaptiveConsensus.js
@@ -1,57 +1,97 @@
 // stable-pi-core/consensus/adaptiveConsensus.js
 
 const { ethers } = require('ethers');
+const EventEmitter = require('events');
 
-class AdaptiveConsensus {
+class AdaptiveConsensus extends EventEmitter {
     constructor() {
+        super();
         this.currentConsensus = 'PoS'; // Default consensus mechanism
-        this.validators = []; // List of validators
-        this.stakes = {}; // Mapping of validators to their stakes
+        this.validators = new Map(); // Map of validators and their stakes
+        this.totalStake = 0; // Total stake in the network
     }
 
     // Function to switch consensus mechanism
     switchConsensus(newConsensus) {
         console.log(`Switching consensus from ${this.currentConsensus} to ${newConsensus}`);
         this.currentConsensus = newConsensus;
+        this.emit('consensusSwitched', newConsensus);
     }
 
     // Function to add a validator
     addValidator(validatorAddress, stake) {
-        if (!this.validators.includes(validatorAddress)) {
-            this.validators.push(validatorAddress);
+        if (!this.validators.has(validatorAddress)) {
+            this.validators.set(validatorAddress, 0);
         }
-        this.stakes[validatorAddress] = (this.stakes[validatorAddress] || 0) + stake;
+        this.validators.set(validatorAddress, this.validators.get(validatorAddress) + stake);
+        this.totalStake += stake;
+        console.log(`Validator ${validatorAddress} added with stake ${stake}. Total stake: ${this.totalStake}`);
+        this.emit('validatorAdded', validatorAddress, stake);
     }
 
     // Function to remove a validator
     removeValidator(validatorAddress) {
-        this.validators = this.validators.filter(v => v !== validatorAddress);
-        delete this.stakes[validatorAddress];
+        if (this.validators.has(validatorAddress)) {
+            const stake = this.validators.get(validatorAddress);
+            this.totalStake -= stake;
+            this.validators.delete(validatorAddress);
+            console.log(`Validator ${validatorAddress} removed. Total stake: ${this.totalStake}`);
+            this.emit('validatorRemoved', validatorAddress);
+        } else {
+            console.log(`Validator ${validatorAddress} not found.`);
+        }
     }
 
     // Function to validate a block
     validateBlock(block) {
-        // Implement validation logic based on the current consensus mechanism
-        if (this.currentConsensus === 'PoS') {
-            return this.validatePoS(block);
-        } else if (this.currentConsensus === 'DPoS') {
-            return this.validateDPoS(block);
+        switch (this.currentConsensus) {
+            case 'PoS':
+                return this.validatePoS(block);
+            case 'DPoS':
+                return this.validateDPoS(block);
+            case 'Lightweight':
+                return this.validateLightweight(block);
+            default:
+                throw new Error('Unknown consensus mechanism');
         }
-        // Add more consensus mechanisms as needed
     }
 
     validatePoS(block) {
-        // Implement PoS validation logic
         console.log('Validating block using PoS');
         // Example: Check if the block is signed by a validator
-        return true; // Placeholder
+        const validator = block.validator; // Assume block has a validator field
+        if (!this.validators.has(validator)) {
+            throw new Error('Invalid validator');
+        }
+        // Additional PoS validation logic can be added here
+        return true; // Placeholder for successful validation
     }
 
     validateDPoS(block) {
-        // Implement DPoS validation logic
         console.log('Validating block using DPoS');
         // Example: Check if the block is signed by a delegate
-        return true; // Placeholder
+        const delegate = block.delegate; // Assume block has a delegate field
+        if (!this.validators.has(delegate)) {
+            throw new Error('Invalid delegate');
+        }
+        // Additional DPoS validation logic can be added here
+        return true; // Placeholder for successful validation
+    }
+
+    validateLightweight(block) {
+        console.log('Validating block using Lightweight consensus');
+        // Lightweight validation logic can be implemented here
+        return true; // Placeholder for successful validation
+    }
+
+    // Function to get the current validators
+    getValidators() {
+        return Array.from(this.validators.keys());
+    }
+
+    // Function to get the total stake
+    getTotalStake() {
+        return this.totalStake;
     }
 }
 
