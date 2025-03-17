853559927c4a653f3452c738cdd6025dc6f46889
KOSASIH <kosasihg88@gmail.com>
2025-03-17 19:31:42 +0700

    Create __init__.py

---
+++ stsp/satellite/__init__.py
@@ -0,0 +1,10 @@
+# stsp/satellite/__init__.py
+
+"""
+Satellite Module for Space-Time Synchronization Protocol (STSP)
+This module handles satellite communication and time synchronization functionalities.
+"""
+
+from .satellite_manager import SatelliteManager
+
+__all__ = ["SatelliteManager"]
