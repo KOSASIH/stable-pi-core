095acfe0297c4d58aa7a28e8a753a8c9e4805e7c
KOSASIH <kosasihg88@gmail.com>
2025-03-17 12:04:52 +0700

    Create __init__.py

---
+++ src/user_interface/__init__.py
@@ -0,0 +1,10 @@
+# src/user_interface/__init__.py
+
+"""
+User  Interface Module for Stable Pi Core
+This module contains user-friendly components for interacting with the system,
+including wallet solutions and a real-time analytics dashboard.
+"""
+
+from .wallet import Wallet
+from .dashboard import Dashboard
