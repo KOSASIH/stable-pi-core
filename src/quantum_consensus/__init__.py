727053c18234c806416c7d03890c4967029946e8
KOSASIH <kosasihg88@gmail.com>
2025-03-17 13:00:19 +0700

    Create __init__.py

---
+++ src/quantum_consensus/__init__.py
@@ -0,0 +1,9 @@
+# src/quantum_consensus/__init__.py
+
+"""
+Quantum Consensus Module for Stable Pi Core
+This module implements the Quantum Entanglement-Based Consensus (QEBC) protocol,
+leveraging quantum properties for secure and instantaneous consensus among nodes.
+"""
+
+from .qebc_protocol import QEBCProtocol
